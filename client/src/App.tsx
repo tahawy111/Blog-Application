c3912a9effc5139f149dafca1e70e2aaafa01aea
tahawy111 <amer.vib@gmail.com>
2022-11-24 21:05:01 +0200

    Done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/home/Home";
+// import { useDispatch, useSelector } from "react-redux";
+// import { RootState } from "./store";
 
 function App() {
-  return <div className="App">Hello</div>;
+  // const dispatch = useDispatch();
+  // const number = useSelector((state: RootState) => state.counter.value);
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <h1
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "calc(100vh - 70px)",
+        color: "#6c757d",
+      }}
+    >
+      404 | Not Found
+    </h1>
+  );
+};
+
+export default NotFound;
