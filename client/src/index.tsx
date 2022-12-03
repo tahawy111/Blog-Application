93f716a31c88ba90225273e11b38185651283da0
tahawy111 <amer.vib@gmail.com>
2022-12-03 20:47:38 +0200

    .

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,6 +16,20 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <ToastContainer
+          position="bottom-right"
+          // autoClose={5000}
+          // hideProgressBar={false}
+          // newestOnTop={false}
+          // closeOnClick
+          // rtl={false}
+          // pauseOnFocusLoss
+          // draggable
+          // pauseOnHover
+          // theme="light"
+        />
+        {/* Same as */}
+        <ToastContainer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
