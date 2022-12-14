2b7f345ba3fdecb2c473ceac15b20837edb89fb8
tahawy111 <amer.vib582@gmail.com>
2022-12-14 22:33:31 +0200

    .

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
-import userSlice from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    user: userSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
