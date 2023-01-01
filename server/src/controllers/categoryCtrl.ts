11de6f9d110787787077814845372a2580eabbb3
tahawy111 <amer.vib@gmail.com>
2023-01-01 15:12:08 +0200

    .

--- server/src/controllers/categoryCtrl.ts
+++ server/src/controllers/categoryCtrl.ts
@@ -1,11 +1,18 @@
 import { Response } from "express";
+import Category from "../models/Category";
 import { IReqAuth } from "./../config/interface";
 
 export const createCategory = async (req: IReqAuth, res: Response) => {
   try {
+    if (!req.user)
+      return res.status(500).json({ msg: "Invalid Authentication." });
+    if (req.user.role !== "admin")
+      return res.status(500).json({ msg: "You Are Not An Admin." });
+
     const name = req.body.name.toLowerCase();
-    console.log({ name });
+
+    const newCat = new Category({ name });
   } catch (error: any) {
-    res.status(500).json({ msg: error.message });
+    return res.status(500).json({ msg: error.message });
   }
 };
