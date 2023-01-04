8beb6c3ea2b5948c210df5f89f363c7a391a1955
tahawy111 <amer.vib582@gmail.com>
2023-01-05 01:21:59 +0200

    .

--- client/src/pages/createBlog/CreateBlog.tsx
+++ client/src/pages/createBlog/CreateBlog.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import Layout from "../../components/Layout";
 
 const CreateBlog = () => {
-  const initialState = new Map(
-    Object.entries({
-      user: "",
-      title: "",
-      content: "",
-      description: "",
-      thumbnail: "",
-      category: "",
-      createdAt: new Date().toISOString(),
-    })
-  );
-  console.log(obj);
+  const initialState = {
+    user: "",
+    title: "",
+    content: "",
+    description: "",
+    thumbnail: "",
+    category: "",
+    createdAt: new Date().toISOString(),
+  };
+
+  const [blog, setBlog] = useState(initialState);
+
   return (
     <Layout>
-      <h1>Create Blog</h1>
+      <div className="my-4 create_blog">
+        <h1>Create Blog</h1>
+
+        <div className="row mt-4">
+          <div className="col-md-6">
+            <h5>Create</h5>
+          </div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
     </Layout>
   );
 };
