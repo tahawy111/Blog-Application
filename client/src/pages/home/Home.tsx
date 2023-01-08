12b1354f6b827d0afef8cc432e21ceafaeb45b9c
tahawy111 <amer.vib@gmail.com>
2023-01-08 22:56:23 +0200

    .

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import Layout from "../../components/Layout";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { ICreateBlogProps } from "../../utils/TypeScript";
+import { IBlogs } from "../../utils/TypeScript";
 
 const Home = () => {
   const { blogs } = useSelector((state: RootState) => state.blog);
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <Layout>
       <div className="home_blogs">
-        {blogs?.map((blog: ICreateBlogProps) => (
-          <div key={blog.}></div>
+        {blogs?.map((blog: IBlogs) => (
+          <div key={blog._id}></div>
         ))}
       </div>
     </Layout>
