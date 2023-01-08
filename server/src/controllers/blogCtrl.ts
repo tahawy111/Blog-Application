0a28927c61e988400c85d14bdaafb4d081e6a954
tahawy111 <amer.vib582@gmail.com>
2023-01-08 02:47:14 +0200

    Finish lesson 28

--- server/src/controllers/blogCtrl.ts
+++ server/src/controllers/blogCtrl.ts
@@ -25,16 +25,85 @@ export const createBlog = async (req: IReqAuth, res: Response) => {
   }
 };
 
-const getHomeBlogs = async (req: IReqAuth, res: Response) => {
+export const getHomeBlogs = async (req: IReqAuth, res: Response) => {
+  // // Example 2
+  // const blogs = await Blog.aggregate([
+  //   // User
+  //   {
+  //     $lookup: {
+  //       from: "users",
+  //       localField: "user",
+  //       foreignField: "_id",
+  //       pipeline: [{ $project: { password: 0 } }],
+  //       as: "user",
+  //     },
+  //   },
+  //   { $unwind: "$user" },
+  // ]);
+
+  // // Example 3
+  // const blogs = await Blog.aggregate([
+  //   // User
+  //   {
+  //     $lookup: {
+  //       from: "users",
+  //       localField: "user",
+  //       foreignField: "_id",
+  //       as: "user",
+  //     },
+  //   },
+  //   { $project: { "user.password": 0 } },
+  //   { $unwind: "$user" },
+  // ]);
+
+  // Example 1
   const blogs = await Blog.aggregate([
     // User
     {
       $lookup: {
-        from: "users",
-        let: { user_id: "${user}" },
-        pipeline: [{ $match: { expr: { $eq: ["$_id", "$$user_id"] } } }],
-        as: "user",
+        from: "users", // here you put the full collection name
+        let: { user_id: "$user" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+          { $project: { password: 0 } },
+        ],
+        as: "user", // here you put the name of the input field
+      },
+    },
+    // array -> object
+    { $unwind: "$user" },
+    // Category
+    {
+      $lookup: {
+        from: "categories",
+        localField: "category",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    // array -> object
+    { $unwind: "$category" },
+    // Sorting
+    { $sort: { createdAt: -1 } },
+    // Group by category
+    {
+      $group: {
+        _id: "$category._id",
+        name: { $first: "$category.name" },
+        blogs: { $push: "$$ROOT" }, // "$$ROOT" refers to the remaining fields from the input document
+        count: { $sum: 1 },
+      },
+    },
+    // Pagination for blogs
+    {
+      $project: {
+        blogs: {
+          $slice: ["$blogs", 0, 4],
+        },
+        count: 1,
+        name: 1,
       },
     },
   ]);
+  res.json(blogs);
 };
--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { auth } from "../middlewares/user";
-import { createBlog } from "./../controllers/blogCtrl";
+import { createBlog, getHomeBlogs } from "./../controllers/blogCtrl";
 
 const router = Router();
 
-router.route("/blog").post(auth, createBlog);
+router.route("/blog").post(auth, createBlog).get(auth, getHomeBlogs);
 
 export default router;
