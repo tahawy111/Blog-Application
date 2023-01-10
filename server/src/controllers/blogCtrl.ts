07fd19dd20542625156e843688d5ebd72dd9c806
tahawy111 <amer.vib582@gmail.com>
2023-01-10 03:52:18 +0200

    Finish #30 [ BE ] GET BLOGS BY CATEGORY

--- server/src/controllers/blogCtrl.ts
+++ server/src/controllers/blogCtrl.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import mongoose from "mongoose";
 import Blog from "../models/Blog";
 import { IReqAuth } from "./../config/interface";
 
@@ -50,6 +51,16 @@ const getAggregate: any = [
     },
   },
 ];
+
+const Pagination = (
+  req: IReqAuth
+): { page: number; limit: number; skip: number } => {
+  let page = Number(req.query.page) * 1 || 1;
+  let limit = Number(req.query.limit) * 1 || 4;
+  let skip = (page - 1) * limit;
+
+  return { page, limit, skip };
+};
 
 export const createBlog = async (req: IReqAuth, res: Response) => {
   if (!req.user)
@@ -109,4 +120,66 @@ export const getHomeBlogs = async (req: IReqAuth, res: Response) => {
   // Example 1
   const blogs = await Blog.aggregate(getAggregate);
   res.json({ blogs });
+};
+export const getBlogsByCategory = async (req: IReqAuth, res: Response) => {
+  try {
+    const { limit, skip } = Pagination(req);
+    const Data = await Blog.aggregate([
+      {
+        $facet: {
+          totalData: [
+            {
+              $match: {
+                category: new mongoose.Types.ObjectId(req.params.category_id),
+              },
+            },
+            {
+              $lookup: {
+                from: "users", // here you put the full collection name
+                let: { user_id: "$user" },
+                pipeline: [
+                  { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+                  { $project: { password: 0 } },
+                ],
+                as: "user", // here you put the name of the input field
+              },
+            },
+
+            // array -> object
+            { $unwind: "$user" },
+            // Sorting
+            { $sort: { createdAt: -1 } },
+            { $skip: skip },
+            { $limit: limit },
+          ],
+          totalCount: [
+            {
+              $match: {
+                category: new mongoose.Types.ObjectId(req.params.category_id),
+              },
+            },
+            { $count: "count" },
+          ],
+        },
+      },
+      {
+        $project: {
+          count: { $arrayElemAt: ["$totalCount.count", 0] },
+          totalData: 1,
+        },
+      },
+    ]);
+
+    const count = Data[0].count;
+    let total: number = 0;
+
+    if (total % limit === 0) {
+      total = count / limit;
+    } else {
+      total = Math.floor(total / limit) + 1;
+    }
+    res.json({ blogs: Data[0].totalData, count, total });
+  } catch (error: any) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/user";
-import { createBlog, getHomeBlogs } from "./../controllers/blogCtrl";
+import {
+  createBlog,
+  getBlogsByCategory,
+  getHomeBlogs,
+} from "./../controllers/blogCtrl";
 
 const router = Router();
 
-router.route("/blog").post(auth, createBlog).get(auth, getHomeBlogs);
+router.route("/blog").post(auth, createBlog).get(getHomeBlogs);
+router.route("/blog/:category_id").get(getBlogsByCategory);
 
 export default router;
